ef6b17742f08631e9fa822748bec9842e09aa99c
mar1kxs <maksim234.kyky@gmail.com>
2025-05-15 18:46:23 +0200

    Lang fix

--- public/src/scripts/lang.js
+++ public/src/scripts/lang.js
@@ -1,11 +1,36 @@
-document.getElementById("languageSelect").addEventListener("change", (e) => {
-  const lang = e.target.value;
-  document.querySelectorAll("[data-ru]").forEach((el) => {
-    if ("placeholder" in el) {
-      el.placeholder = el.getAttribute(`data-${lang}`) || el.placeholder;
-    }
-    if (el.tagName !== "INPUT" && el.tagName !== "TEXTAREA") {
-      el.innerHTML = el.getAttribute(`data-${lang}`) || el.innerHTML;
-    }
+const switchLang = (lang) => {
+  localStorage.setItem("selectedLang", lang);
+
+  document.querySelectorAll(`[data-${lang}]`).forEach((el) => {
+    el.innerHTML = el.getAttribute(`data-${lang}`);
   });
+
+  document.querySelectorAll(`[data-placeholder-${lang}]`).forEach((input) => {
+    input.placeholder = input.getAttribute(`data-placeholder-${lang}`);
+  });
+};
+
+document
+  .querySelectorAll(".lang-switch button, #languageSelect")
+  .forEach((el) => {
+    el.addEventListener("click", (e) => {
+      const lang = e.target.value || el.getAttribute("data-lang");
+      switchLang(lang);
+    });
+
+    el.addEventListener("change", (e) => {
+      const lang = e.target.value;
+      switchLang(lang);
+    });
+  });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedLang = localStorage.getItem("selectedLang") || "en";
+  const langSelect = document.querySelector("#languageSelect");
+
+  if (langSelect) {
+    langSelect.value = savedLang;
+  }
+
+  switchLang(savedLang);
 });
